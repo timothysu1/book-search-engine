55c69e9945fabf630888e4cb2a8402850800d649
Timothy <timsu100@gmail.com>
2023-09-06 20:58:34 -0700

    refactor loginform and signupform

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
 //import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +31,19 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      /* const response = await createUser(userFormData); */
 
-      if (!response.ok) {
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      })
+
+      if (!data.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      //const { token, user } = await response.json();
+      //console.log(user);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
